d63d06b0b40e34a2a663f57433d8b0f4ace2234d
Biplob Hasan Nibir <bh.nibir@gmail.com>
2023-11-01 00:26:28 +0600

    grid layout design done

--- src/App.jsx
+++ src/App.jsx
@@ -30,19 +30,43 @@ const images = [
     image: "../src/assets/images/image-10.jpeg",
   },
   {
+    future: true,
     image: "../src/assets/images/image-11.jpeg",
   },
 ];
 
 function App() {
   return (
-    <>
+    <div className="container mx-auto px-6">
       <h1 className="text-3xl font-bold  text-purple-600">Image Galary</h1>
 
-      {images.map((item, index) => (
-        <img key={index} src={`${item.image}`} />
-      ))}
-    </>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4">
+        {images.map((item, index) =>
+          item.future ? (
+            <div
+              key={index}
+              className="order-first md:col-span-2 md:row-span-2"
+            >
+              <img
+                className="max-w-full max-h-full rounded-lg border-solid border-2 border-sky-500"
+                src={`${item.image}`}
+              />
+            </div>
+          ) : (
+            <div key={index}>
+              <img
+                className="max-w-full max-h-full rounded-lg border-solid border-2 border-sky-500"
+                src={`${item.image}`}
+              />
+            </div>
+          )
+        )}
+
+        <div className="order-last rounded-lg border-solid border-2 border-sky-500">
+          Add Image
+        </div>
+      </div>
+    </div>
   );
 }
 
