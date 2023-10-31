c89c8da5dbf13c6d88ebd7f987370deaa5b698a2
Biplob Hasan Nibir <bh.nibir@gmail.com>
2023-10-31 23:52:22 +0600

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,49 @@
-import './App.css'
+const images = [
+  {
+    image: "../src/assets/images/image-1.webp",
+  },
+  {
+    image: "../src/assets/images/image-2.webp",
+  },
+  {
+    image: "../src/assets/images/image-3.webp",
+  },
+  {
+    image: "../src/assets/images/image-4.webp",
+  },
+  {
+    image: "../src/assets/images/image-5.webp",
+  },
+  {
+    image: "../src/assets/images/image-6.webp",
+  },
+  {
+    image: "../src/assets/images/image-7.webp",
+  },
+  {
+    image: "../src/assets/images/image-8.webp",
+  },
+  {
+    image: "../src/assets/images/image-9.webp",
+  },
+  {
+    image: "../src/assets/images/image-10.jpeg",
+  },
+  {
+    image: "../src/assets/images/image-11.jpeg",
+  },
+];
 
 function App() {
-
   return (
-    <h1 className="text-3xl font-bold underline text-purple-600">
-      Hello world!
-    </h1>
-  )
+    <>
+      <h1 className="text-3xl font-bold  text-purple-600">Image Galary</h1>
+
+      {images.map((item, index) => (
+        <img key={index} src={`${item.image}`} />
+      ))}
+    </>
+  );
 }
 
-
-export default App
+export default App;
