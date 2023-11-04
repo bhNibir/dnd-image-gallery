9ec6754656ce1d66e2e84877fa95af890f92292d
Biplob Hasan Nibir <bh.nibir@gmail.com>
2023-11-05 00:27:49 +0600

    deleted unused files

--- src/components1/index.ts
+++
@@ -1,6 +0,0 @@
-
-export {Item, Action, Handle, Remove} from './Item';
-export {GridContainer} from './GridContainer';
-export {List} from './List';
-export {OverflowWrapper} from './OverflowWrapper';
-export {Wrapper} from './Wrapper';
